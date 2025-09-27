d4c40fa11e40b5e7d31de0ecb4694b30db9ef286
JohnLiu <rockstargames@live.hk>
2025-09-27 13:51:58 +0800

    - initial commit

---
+++ StaffManagementApp/StaffManagementApp/StaffManagementAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StaffManagementApp.swift
+//  StaffManagementApp
+//
+//  Created by John Liu on 27/9/2025.
+//
+
+import SwiftUI
+
+@main
+struct StaffManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
